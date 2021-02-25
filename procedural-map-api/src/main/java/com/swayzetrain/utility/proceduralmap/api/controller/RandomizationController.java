5374b2ea9a5b6ffea93b783f0bf31a8c544a3b0e
Joe Castle <brocatles@gmail.com>
2021-02-25 03:24:25 -0500

    Adding new controller method to get back a random coordinate of a specific TileType when passing in the mapData object

--- procedural-map-api/src/main/java/com/swayzetrain/utility/proceduralmap/api/controller/RandomizationController.java
+++ procedural-map-api/src/main/java/com/swayzetrain/utility/proceduralmap/api/controller/RandomizationController.java
@@ -1,16 +1,25 @@
 package com.swayzetrain.utility.proceduralmap.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.swayzetrain.utility.proceduralmap.api.service.MapUtilityService;
 import com.swayzetrain.utility.proceduralmap.common.enums.Direction;
+import com.swayzetrain.utility.proceduralmap.common.enums.TileType;
+import com.swayzetrain.utility.proceduralmap.common.model.Coordinate;
 import com.swayzetrain.utility.proceduralmap.common.service.RandomizationService;
 
 @RestController
 public class RandomizationController {
+	
+	@Autowired
+	MapUtilityService mapUtilityService;
 
 	@GetMapping("/v1/randomization/directions")
 	public ResponseEntity<Direction> getRandomDirection(@RequestParam(required=true) Long seed) {
@@ -24,6 +33,11 @@ public class RandomizationController {
 		RandomizationService randomizationService = new RandomizationService(seed);
 		
 		return new ResponseEntity<>(randomizationService.generateRandomIntegerExcludingZero(maxLength), HttpStatus.OK);
+	}
+	
+	@PostMapping("/v1/randomization/path-coordinates")
+	public ResponseEntity<Coordinate> createRandomPathCoordinate(@RequestBody Integer[][] map) {
+		return new ResponseEntity<>(mapUtilityService.findCoordinateofGivenType(map, TileType.PATH), HttpStatus.OK);
 	}
 	
 }
--- procedural-map-api/src/main/java/com/swayzetrain/utility/proceduralmap/api/service/MapUtilityService.java
+++ procedural-map-api/src/main/java/com/swayzetrain/utility/proceduralmap/api/service/MapUtilityService.java
@@ -1,5 +1,7 @@
 package com.swayzetrain.utility.proceduralmap.api.service;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Service;
 
 import com.swayzetrain.utility.proceduralmap.common.enums.TileType;
@@ -10,23 +12,35 @@ import com.swayzetrain.utility.proceduralmap.common.service.RandomizationService
 public class MapUtilityService {
 
 	public void buryTreasureInMap(Integer[][] map, Integer treasureCount, RandomizationService randomizationService) {
-		for(int treasuresBurried = 0; treasuresBurried < treasureCount; treasuresBurried++) {
+		for (int treasuresBurried = 0; treasuresBurried < treasureCount; treasuresBurried++) {
 			Coordinate coordinate = findCoordinateofGivenType(map, randomizationService, TileType.PATH);
 			map[coordinate.getY()][coordinate.getX()] = TileType.PATH_TREASURE.getValue();
 		}
 	}
-	
-	public Coordinate findCoordinateofGivenType(Integer[][] map, RandomizationService randomizationService, TileType tileType) {
-		
+
+	public Coordinate findCoordinateofGivenType(Integer[][] map, TileType tileType) {
+
+		RandomizationService randomizationService = new RandomizationService(new Random().nextLong());
+
 		Coordinate coordinate = randomizationService.generateRandomCoordinate(map[0].length, map.length);
-		
-		if(map[coordinate.getY()][coordinate.getX()] == tileType.getValue()) {
+
+		if (map[coordinate.getY()][coordinate.getX()] == tileType.getValue()) {
+			return coordinate;
+		} else {
+			return findCoordinateofGivenType(map, tileType);
+		}
+	}
+
+	public Coordinate findCoordinateofGivenType(Integer[][] map, RandomizationService randomizationService,
+			TileType tileType) {
+
+		Coordinate coordinate = randomizationService.generateRandomCoordinate(map[0].length, map.length);
+
+		if (map[coordinate.getY()][coordinate.getX()] == tileType.getValue()) {
 			return coordinate;
 		} else {
 			return findCoordinateofGivenType(map, randomizationService, tileType);
 		}
 	}
-	
-	
-	
+
 }
